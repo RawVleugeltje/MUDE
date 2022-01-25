d23503665bfa72652ca038b156cb9886e8b14388
RawVleugeltje <91325499+RawVleugeltje@users.noreply.github.com>
2022-01-25 15:41:31 +0100

    Update BearingCapacity.py

--- BearingCapacity.py
+++ BearingCapacity.py
@@ -2,5 +2,6 @@ def MaxTensileForce(length, resistance, f1, f2, Op_mean, alpha):
   return resistance*f1*f2*Op_mean*alpha
 
 def IntTensileForce(length, resistance, f1, f2, Op_mean, alpha):
+  from scipy.integrate import quad
   intF = quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,Op_mean,alpha))
   return intF
