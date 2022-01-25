7d6b6ee512046898655e01578605a356bf1fc95f
RawVleugeltje <91325499+RawVleugeltje@users.noreply.github.com>
2022-01-25 15:12:00 +0100

    Delete BC2.py

--- BC2.py
+++
@@ -1,6 +0,0 @@
-def MaxTensileForce(length, resistance, f1, f2, Op_mean, alpha):
-  return resistance*f1*f2*Op_mean*alpha
-
-def IntTensileForce(length, resistance, f1, f2, Op_mean, alpha):
-  intF = quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,Op_mean,alpha))
-  return intF
