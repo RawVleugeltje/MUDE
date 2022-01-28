f09af627a587716c7974da6fdac8857259e67edf
RawVleugeltje <91325499+RawVleugeltje@users.noreply.github.com>
2022-01-28 12:25:23 +0100

    Update BearingCapacity.py

--- BearingCapacity.py
+++ BearingCapacity.py
@@ -1,26 +1,26 @@
 def MaxTensileForce(length, resistance, f1, f2, Op_mean, alpha):
-  return resistance*f1*f2*Op_mean*alpha
+    return resistance*f1*f2*Op_mean*alpha
 
 def IntTensileForce(length, resistance, f1, f2, width, alpha, plot=False):
-  from scipy.integrate import quad
-  import matplotlib.pyplot as plt
-  import numpy as np
+    from scipy.integrate import quad
+    import matplotlib.pyplot as plt
+    import numpy as np
   
-  Op_mean = 4*width
-  intF = quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,Op_mean,alpha))[0]
+    Op_mean = 4*width
+    intF = quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,Op_mean,alpha))[0]
     
-  width_line = []
-  resistance_line = []
-  length_line = []
-  width_number = np.linspace(0,1.1,100)
-  resistance_number = np.linspace(9,21,100)
-  length_number = np.linspace(0,11,100)
+    width_line = []
+    resistance_line = []
+    length_line = []
+    width_number = np.linspace(0,1.1,100)
+    resistance_number = np.linspace(9,21,100)
+    length_number = np.linspace(0,11,100)
   
-  if plot == True:
-    for i in range(100):
-      width_line.append(quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,width_number[i]*4,alpha))[0])
-      resistance_line.append(quad(MaxTensileForce,0,length,args=(resistance_number[i]*1000,f1,f2,width*4,alpha))[0])
-      length_line.append(quad(MaxTensileForce,0,length_number[i],args=(resistance*1000,f1,f2,width*4,alpha))[0])
+    if plot == True:
+        for i in range(100):
+            width_line.append(quad(MaxTensileForce,0,length,args=(resistance*1000,f1,f2,width_number[i]*4,alpha))[0])
+            resistance_line.append(quad(MaxTensileForce,0,length,args=(resistance_number[i]*1000,f1,f2,width*4,alpha))[0])
+            length_line.append(quad(MaxTensileForce,0,length_number[i],args=(resistance*1000,f1,f2,width*4,alpha))[0])
   
     fig, ax = plt.subplots(1,3,figsize=(12,4))
     ax[0].plot(width_line,width_number,'r')
@@ -76,36 +76,36 @@ def ClumpCriterion(width, length, r_grid, v_soil, v_water, v_concrete):
     return F_tension_max
 
 def ClumpCriterionFig(width, length, r_grid, v_soil, v_water, v_concrete):
-  import numpy as np
-  import matplotlib.pyplot as plt
+    import numpy as np
+    import matplotlib.pyplot as plt
   
-  width_line = []
-  length_line = []
-  width_number = np.linspace(0,0.75,100)
-  length_number = np.linspace(0,11,100)
+    width_line = []
+    length_line = []
+    width_number = np.linspace(0,0.75,100)
+    length_number = np.linspace(0,11,100)
       
-  for i in range(100):
-    width_line.append(ClumpCriterion(width_number[i],length,r_grid,v_soil,v_water,v_concrete))
-    length_line.append(ClumpCriterion(width,length_number[i],r_grid,v_soil,v_water,v_concrete))
+    for i in range(100):
+        width_line.append(ClumpCriterion(width_number[i],length,r_grid,v_soil,v_water,v_concrete))
+        length_line.append(ClumpCriterion(width,length_number[i],r_grid,v_soil,v_water,v_concrete))
   
-  fig, ax = plt.subplots(1,2,figsize=(12,4))
-  ax[0].plot(width_line,width_number,'r')
-  ax[0].plot(ClumpCriterion(width,length,r_grid,v_soil,v_water,v_concrete),width,'r.',markersize=30)
-  ax[0].set_xlabel('Tensile Force [kN]')
-  ax[0].set_ylabel('Width [m]')
-  ax[0].set_ylim(0,0.8)
-  ax[0].set_xlim(-10,800)
-  ax[0].grid()
+    fig, ax = plt.subplots(1,2,figsize=(12,4))
+    ax[0].plot(width_line,width_number,'r')
+    ax[0].plot(ClumpCriterion(width,length,r_grid,v_soil,v_water,v_concrete),width,'r.',markersize=30)
+    ax[0].set_xlabel('Tensile Force [kN]')
+    ax[0].set_ylabel('Width [m]')
+    ax[0].set_ylim(0,0.8)
+    ax[0].set_xlim(-10,800)
+    ax[0].grid()
   
-  ax[1].plot(length_line,length_number,'b')
-  ax[1].plot(ClumpCriterion(width,length,r_grid,v_soil,v_water,v_concrete),length,'b.',markersize=30)
-  ax[1].set_xlabel('Tensile Force [kN]')
-  ax[1].set_ylabel('Length [m]')
-  ax[1].set_ylim(0,11)
-  ax[1].set_xlim(-10,850)
-  ax[1].grid()
+    ax[1].plot(length_line,length_number,'b')
+    ax[1].plot(ClumpCriterion(width,length,r_grid,v_soil,v_water,v_concrete),length,'b.',markersize=30)
+    ax[1].set_xlabel('Tensile Force [kN]')
+    ax[1].set_ylabel('Length [m]')
+    ax[1].set_ylim(0,11)
+    ax[1].set_xlim(-10,850)
+    ax[1].grid()
       
-  plt.tight_layout()
+    plt.tight_layout()
     
   return 
   
