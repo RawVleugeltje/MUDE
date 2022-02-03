b5985215c90e30366aeeea9276d851e68a107e4f
RawVleugeltje <91325499+RawVleugeltje@users.noreply.github.com>
2022-02-03 17:17:32 +0100

    Update DynamicWindPressure.py

--- DynamicWindPressure.py
+++ DynamicWindPressure.py
@@ -18,25 +18,24 @@ def TheoreticalWindPlot(tau_w, rho, k, z, z0):
     tau_line.append(TheoreticalWind(tau_number[i],rho,k,z,z0))
     z_line.append(TheoreticalWind(tau_number,rho,k,z_number[i],z0))
   
-  if plot == True:
-    fig, ax = plt.subplots(1,2,figsize=(12,4))
+  fig, ax = plt.subplots(1,2,figsize=(12,4))
     
-    ax[0].plot(tau_line, tau_number)
-    ax[0].plot(p_w,tau_w,'r.',markersize=30)
-    ax[0].set_xlabel('Dynamic wind pressure [Pa]')
-    ax[0].set_ylabel('Surface shear stress [Pa]')
-    ax[0].set_ylim(0,1.1)
-    ax[0].set_xlim(0,102)
-    ax[0].grid()
+  ax[0].plot(tau_line, tau_number)
+  ax[0].plot(p_w,tau_w,'r.',markersize=30)
+  ax[0].set_xlabel('Dynamic wind pressure [Pa]')
+  ax[0].set_ylabel('Surface shear stress [Pa]')
+  ax[0].set_ylim(0,1.1)
+  ax[0].set_xlim(0,102)
+  ax[0].grid()
   
-    ax[1].plot(z_line, z_number)
-    ax[1].plot(p_w,z,'b.',markersize=30)
-    ax[1].set_xlabel('Dynamic wind pressure [Pa]')
-    ax[1].set_ylabel('Elevation [m]')
-    ax[1].set_ylim(0,510)
-    ax[1].set_xlim(0.102)
-    ax[1].grid()
+  ax[1].plot(z_line, z_number)
+  ax[1].plot(p_w,z,'b.',markersize=30)
+  ax[1].set_xlabel('Dynamic wind pressure [Pa]')
+  ax[1].set_ylabel('Elevation [m]')
+  ax[1].set_ylim(0,510)
+  ax[1].set_xlim(0.102)
+  ax[1].grid()
     
-    plt.tight_layout()
+  plt.tight_layout()
   
   return
